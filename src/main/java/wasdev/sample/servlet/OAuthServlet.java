c8e35ef00912f75644062308a7c9319e7089e495
xiaolixu <xiaolixu@cn.ibm.com>
2016-12-09 10:04:38 +0000

    update code for bluemix runtime

--- src/main/java/wasdev/sample/servlet/OAuthServlet.java
+++ src/main/java/wasdev/sample/servlet/OAuthServlet.java
@@ -1,30 +1,32 @@
 package wasdev.sample.servlet;
 //package com.mcademo;
 
-import okhttp3.*;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.json.JSONObject;
-import net.iharder.Base64;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-//import java.util.Base64;
 import java.util.logging.Logger;
 
 public class OAuthServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
     // Define properties required for OAuth flows
     private static final Logger logger = Logger.getLogger(OAuthServlet.class.getName());
-    private static final OkHttpClient client = new OkHttpClient();
-
+    
     private static final String clientId = "4b07cfe1-3875-419c-a0a2-8fb0a7b43dff";
     private static final String clientSecret = "MDlkYjZkYjAtOGVkOS00MjcxLWE5ZTMtMWNlODVhMDQ0MDUy";
     private static final String callbackUri = "http://mca-java.mybluemix.net/oauth/callback";
     private static final String authzEndpoint = "https://mobileclientaccess.ng.bluemix.net/oauth/v2/authorization";
     private static final String tokenEndpoint = "https://mobileclientaccess.ng.bluemix.net/oauth/v2/token";
-
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
@@ -67,30 +69,37 @@ public class OAuthServlet extends HttpServlet {
         } catch (Throwable t){}
 
 
-        String credentials = Credentials.basic(clientId, clientSecret);
-        Request req = new Request.Builder()
-                .url(tokenEndpoint)
-                .header("Authorization", credentials)
-                .post(RequestBody.create(MediaType.parse("application/json"), body.toString()))
-                .build();
+        HttpPost req = new HttpPost(tokenEndpoint);
+        String credentials = clientId + ":" + clientSecret;
+        req.setHeader("Authorization", "Basic " + Base64.encodeBase64String(credentials.getBytes()));
+        StringEntity entity = new StringEntity(body.toString(), ContentType.APPLICATION_JSON);
+        req.setEntity(entity);
 
+        CloseableHttpClient client = HttpClients.createDefault();
+        CloseableHttpResponse resp = client.execute(req);
+        String responseBodyString = EntityUtils.toString(resp.getEntity());
 
-        Response res = client.newCall(req).execute();
-        JSONObject parsedBody = new JSONObject(res.body().string());
-        String accessToken = parsedBody.getString("access_token");
-        String idToken = parsedBody.getString("id_token");
+        JSONObject responseBodyJSON = new JSONObject(responseBodyString);
 
-        //byte[] decodedIdTokenPayload = Base64.getUrlDecoder().decode(idToken.split("\\.")[1]);
-        byte[] decodedIdTokenPayload = Base64.decode(idToken.split("\\.")[1], Base64.URL_SAFE);
-        JSONObject decodedIdentityToken = new JSONObject(new String(decodedIdTokenPayload));
-        JSONObject userIdentity = decodedIdentityToken.getJSONObject("imf.user");
+        String accessTokenBase64 = responseBodyJSON.getString("access_token");
+        String idTokenBase64 = responseBodyJSON.getString("id_token");
+        String idTokenBase64Payload = idTokenBase64.split("\\.")[1];
+
+        byte[] idTokenDecodedPayload = new Base64(true).decodeBase64(idTokenBase64Payload);
+
+        String idTokenDecodedPayloadString = new String(idTokenDecodedPayload);
+        JSONObject idTokenJSON = new JSONObject(idTokenDecodedPayloadString);
+        JSONObject userIdentity = idTokenJSON.getJSONObject("imf.user");
 
         JSONObject authData = new JSONObject();
-        authData.put("accessToken", accessToken);
-        authData.put("idToken", idToken);
+        authData.put("accessToken", accessTokenBase64);
+        authData.put("idToken", idTokenBase64);
         authData.put("userIdentity", userIdentity);
 
         request.getSession().setAttribute("mca", authData);
+
+        resp.close();
+        client.close();
 
         response.sendRedirect("/");
     }
